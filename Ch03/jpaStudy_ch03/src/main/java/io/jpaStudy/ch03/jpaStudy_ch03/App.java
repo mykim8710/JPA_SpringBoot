e5b6b72833fe6a19b75f0957cc5166d1d55fc902
mykim2020 <mykim8710@gmail.com>
2020-05-22 18:50:23 +0900

    ch03 jpa 코드

---
+++ Ch03/jpaStudy_ch03/src/main/java/io/jpaStudy/ch03/jpaStudy_ch03/App.java
@@ -0,0 +1,11 @@
+package io.jpaStudy.ch03.jpaStudy_ch03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
